52059ebf6cfa9336e3c6573bdb5af4af63bc8711
long nguyen <nguyentienlong88@gmail.com>
2016-05-11 18:22:39 +0700

    fix php cs

--- src/ClassNeedTest.php
+++ src/ClassNeedTest.php
@@ -10,13 +10,13 @@ class ClassNeedTest
     {
         $periodList = [];
         $now = Carbon::now();
-        $dateTo     = $now->day > 15 ? Carbon::create($now->year, $now->month, 15, 23, 59, 59)->addMonth() :
+        $dateTo = $now->day > 15 ? Carbon::create($now->year, $now->month, 15, 23, 59, 59)->addMonth() :
             Carbon::create($now->year, $now->month, 15, 23, 59, 59);
 
-        for ($i = 0; $i < $periods; $i++) {
-            $dateFrom     = $dateTo->copy()->subMonth()->addDay(1)->setTime(0, 0, 0);
+        for ($i = 0; $i < $periods; ++$i) {
+            $dateFrom = $dateTo->copy()->subMonth()->addDay(1)->setTime(0, 0, 0);
             $periodList[] = ['from' => $dateFrom, 'to' => $dateTo];
-            $dateTo       = $dateFrom->copy()->subDay(1)->setTime(23, 59, 59);
+            $dateTo = $dateFrom->copy()->subDay(1)->setTime(23, 59, 59);
         }
 
         return $periodList;
--- test/TestClassNeedToTest.php
+++ test/TestClassNeedToTest.php
@@ -1,34 +1,30 @@
 <?php
 
-namespace Test;
+namespace test;
 
 use LongKa\ClassNeedTest;
-
 use Carbon\Carbon;
 
 class TestClassNeedToTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetPayoutPeriods()
     {
-
         $from = Carbon::create(2016, 04, 16, 00);
         $to = Carbon::create(2016, 05, 15, 23, 59, 59);
 
         $item = ['from' => $from, 'to' => $to];
 
-        $expected = [ $item ];
+        $expected = [$item];
 
         $today = Carbon::create(2016, 05, 11, 00);
         Carbon::setTestNow($today);
 
-        $classNeedTest = new ClassNeedTest;
-
+        $classNeedTest = new ClassNeedTest();
 
         $result = $classNeedTest->getPayoutPeriods(1);
 
-        $this->assertTrue($expected == $result);
-
         Carbon::setTestNow();
 
+        $this->assertTrue($expected == $result);
     }
 }
